15f75ca691013600480fd0e26967a889df896389
Lord-Giganticus <mrubins64@gmail.com>
2022-01-31 16:13:40 -0600

    prep for update

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::path::*;
 use std::fs::*;
 use std::io::*;
+use std::str;
 
 mod getfile;
 mod yaz0;
@@ -11,12 +12,17 @@ fn main() {
     let args = &envargs[1..];
     for arg in args {
         let mut buffer = getfile::get_file_as_byte_vec(arg);
-        buffer = yaz0::yaz0decomp(&buffer);
-        let mut name = Path::new(arg).file_stem().unwrap().to_str().unwrap().to_owned();
-        let ext = Path::new(arg).extension().unwrap().to_str().unwrap();
-        name.push_str(".decomp.");
-        name.push_str(ext);
-        let mut file = File::create(name).unwrap();
-        file.write(&buffer).expect("Write Failed:");
+        let magic = str::from_utf8(&buffer[0..4]).unwrap();
+        if magic == "Yaz0" {
+            buffer = yaz0::yaz0decomp(&buffer);
+            let mut name = Path::new(arg).file_stem().unwrap().to_str().unwrap().to_owned();
+            let ext = Path::new(arg).extension().unwrap().to_str().unwrap();
+            name.push_str(".decomp.");
+            name.push_str(ext);
+            let mut file = File::create(name).unwrap();
+            file.write(&buffer).expect("Write Failed:");
+        } else {
+            
+        }
     }
 }
